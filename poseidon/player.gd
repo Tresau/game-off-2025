a7a2b380d75db00a21ade663f4d885e8120162bb
Timothy <timothy.esau22@gmail.com>
2025-11-11 11:25:37 -0500

    phyics player in progress, joints

---
+++ poseidon/player.gd
@@ -0,0 +1,17 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("left_oar"):
+		print("left oar triggered")
+	if Input.is_action_just_pressed("right_oar"):
+		print("right oar triggered")
+	
+	pass
